63396f3792ee4dc1188d2320a5ce3145154799c2
Evgeny V. Bogodukhov <b@evbg.ru>
2020-01-28 14:22:27 +0400

    modified:   bemacs-layers/bemacs/layers.el

--- bemacs-layers/bemacs/layers.el
+++ bemacs-layers/bemacs/layers.el
@@ -1,3 +1,5 @@
 (configuration-layer/declare-layers '(bemacs-base
+                                      bemacs-clojure
+                                      bemacs-python
                                       bemacs-s
                                       bemacs-M))
