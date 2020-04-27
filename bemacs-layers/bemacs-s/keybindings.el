74b4a5bcbd2ebbcbb35f0a422d15afdc4d7bd15f
Evgeny V. Bogodukhov <b@evbg.ru>
2020-04-27 15:16:12 +0400

    modified:   bemacs-layers/bemacs-s/keybindings.el

--- bemacs-layers/bemacs-s/keybindings.el
+++ bemacs-layers/bemacs-s/keybindings.el
@@ -7,11 +7,11 @@
 (global-unset-key (kbd "s-b"))
 (global-set-key (kbd "s-b") 'helm-buffers-list)
 
-(global-unset-key (kbd "s-9"))
-(global-set-key (kbd "s-9") 'previous-buffer)
+(global-unset-key (kbd "C-s-9"))
+(global-set-key (kbd "C-s-9") 'previous-buffer)
 
-(global-unset-key (kbd "s-0"))
-(global-set-key (kbd "s-0") 'next-buffer)
+(global-unset-key (kbd "C-s-0"))
+(global-set-key (kbd "C-s-0") 'next-buffer)
 
 (global-unset-key (kbd "s-j"))
 (global-set-key (kbd "s-j") 'evil-scroll-down)
@@ -28,14 +28,44 @@
 (global-unset-key (kbd "s-w"))
 (global-set-key (kbd "s-w") 'kill-buffer-and-window)
 
+(global-unset-key (kbd "C-s-3"))
+(global-set-key (kbd "C-s-3") 'split-window-right)
+
+(global-unset-key (kbd "C-s-2"))
+(global-set-key (kbd "C-s-2") 'split-window-below)
+
+(global-unset-key (kbd "C-s-1"))
+(global-set-key (kbd "C-s-1") 'spacemacs/toggle-maximize-buffer)
+
 (global-unset-key (kbd "s-1"))
-(global-set-key (kbd "s-1") 'spacemacs/toggle-maximize-buffer)
+(global-set-key (kbd "s-1") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-1-and-exit)
 
 (global-unset-key (kbd "s-2"))
-(global-set-key (kbd "s-2") 'split-window-below)
+(global-set-key (kbd "s-2") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-2-and-exit)
 
 (global-unset-key (kbd "s-3"))
-(global-set-key (kbd "s-3") 'split-window-right)
+(global-set-key (kbd "s-3") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-3-and-exit)
+
+(global-unset-key (kbd "s-4"))
+(global-set-key (kbd "s-4") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-4-and-exit)
+
+(global-unset-key (kbd "s-5"))
+(global-set-key (kbd "s-5") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-5-and-exit)
+
+(global-unset-key (kbd "s-6"))
+(global-set-key (kbd "s-6") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-6-and-exit)
+
+(global-unset-key (kbd "s-7"))
+(global-set-key (kbd "s-7") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-7-and-exit)
+
+(global-unset-key (kbd "s-8"))
+(global-set-key (kbd "s-8") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-8-and-exit)
+
+(global-unset-key (kbd "s-9"))
+(global-set-key (kbd "s-9") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-9-and-exit)
+
+(global-unset-key (kbd "s-0"))
+(global-set-key (kbd "s-0") 'spacemacs/layouts-transient-state/spacemacs/persp-switch-to-0-and-exit)
 
 (global-unset-key (kbd "s-`"))
 (global-set-key (kbd "s-`") 'other-window)
